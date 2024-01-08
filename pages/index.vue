b1975e98d3f00e281af1e476fbe52965af405c11
gregor <e@tokarev.work>
2024-01-08 12:37:32 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>page index</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#060606",
+        "gray-400": "#4D4D4D",
+        "gray-300": "#A1A1A1",
+        "gray-200": "#BDBDBD",
+        "gray-100": "#E8E8E8",
+        white: "#FFFFFF",
+        "white-hover": "#FAFAFA",
+      },
+    },
+  },
+  plugins: [],
+};
